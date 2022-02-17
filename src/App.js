f33390cc601ce326edb45eeb851d6790ce121b14
Violeta <missviolets13@gmail.com>
2022-02-17 16:43:47 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
+import { info } from './data';
 import './App.css';
 
 function App() {
+
+  const [hotel, setHotel] = useState(data);
+  const [resto, setResto] = useState(0);
+  const {number, place, restaurant} = info[resto];
+
+  const PreviousRestaurant = () => {
+    setResto((resto => {
+     resto --;
+      if (resto < 0) {
+        return data.length -1;
+      }
+      return resto;
+    }))
+   }
+ 
+   const NextRestaurant = () => {
+    setResto((resto => {
+     resto ++;
+      if (resto > data.length - 1) {
+        resto = 0;
+     }
+      return resto;
+    }))
+   }
+
+  const removeHotel = (id) => {
+    let newHotels = hotel.filter( element => element.id !== id);
+    setHotel(newHotels);
+  }
+
+  const [showMore, setShowMore] = useState(false);
+
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <div className='container'>
+           <h1>Must live {hotel.length} hotels in Paris! </h1>
+        </div>
+        {hotel.map((hotels => {
+           const {id, name, description, image, address} = hotels;
+           return(
+             <div key={id}>
+            <div className='container'>
+            <h2>{id} - {name} </h2>
+         </div>
+         <div className='container'>
+            <p>{showMore ? description : description.substring(0,200) + "...."} 
+            <button onClick={() => setShowMore (!showMore)}>{showMore ? "Show less" : "Show more"}</button></p>
+         </div>
+         <div className='container btnnew'>
+            <img alt='hotelphoto' width="500px" src={image} />
+         </div>
+         <div className='container'>
+            <h4> Address: {address}</h4>
+         </div>
+         <div className='container'>
+            <button className='btn' onClick={() => removeHotel(id) }>Remove</button>
+         </div>
+       
+
+
+         </div>
+           )
+        }))}
+
+        <div className='container'>
+            <button className='btn class' onClick={()=> setHotel([])}>Remove All</button>
+        </div>
+        <div className='container'>
+           <h2>Top restaurants in Paris!</h2>
+        </div>
+        <div className='container'>
+           <img alt='rest' width="500px" src={restaurant} />
+        </div>
+        <div className='container'>
+          <h3>No {number} - {place}</h3>
+        </div>
+        <div className='container'>
+          <button className='prev btnnew' onClick={PreviousRestaurant}>Previous</button>
+          <button className='prev btnnew' onClick={NextRestaurant}>Next</button>
+        </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const data = [
+
+ {
+     id: 1, 
+     name: "Le Bristol Paris",
+     description: "An icon of French elegance and art de vivre, Le Bristol Paris is located at one of the city’s most prestigious addresses on rue du Faubourg Saint-Honoré. The first to receive France’s “Palace” distinction, the hotel boasts 190 rooms and suites— among the largest in Paris — that are now completely renovated. An exceptional setting offering Oetker Collection’s legendary hospitality, Le Bristol is perhaps best known for its outstanding dining tradition, holding 4 Michelin stars between Epicure and 114 Faubourg. A spirit of joie de vivre permeates the hotel from early morning to after midnight with relaxation in the lush, 13,000 square foot courtyard garden, teatime at Café Antonia, wellbeing and fitness at Spa Le Bristol by La Prairie and the iconic rooftop swimming pool, and captivating evenings at Le Bar du Bristol.",
+     image: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/14/ec/1f/42/le-bristol-paris-colonnade.jpg?w=700&h=-1&s=1",
+     address: "112 rue du Faubourg Saint Honore 8th Arr., 75008 Paris France"
+ },
+
+ {
+    id: 2, 
+    name: "Hotel Malte - Astotel",
+    description: "Located in the 2nd district next to the Stock Exchange, the Louvre and Place des Victoires, Hotel Malta provides a superb location for a memorable stay. Whether you want to shop in the heart of the department store neighbourhood or visit one of the main business centres of Paris, Malta Hotel is the place to be. Elegantly concealed behind a facade dating from the seventeenth century, let yourself be seduced by our garden patio, a pleasurable experience on sunny days, our cozy breakfast room, and the relaxing atmosphere of our lobby. After the warm welcome by our professional team, always at your disposal, you will appreciate the comfort of our rooms decorated in an elegant and timeless design.",
+    image: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/6d/c4/cd/superior-double-room.jpg?w=1600&h=-1&s=1",
+    address: "63 rue de Richelieu, 75002 Paris France"
+},
+
+{
+    id: 3, 
+    name: "Hotel Georgette",
+    description: "In the Marais, close to Beaubourg.In the heart of the Marais, a short walk from the Georges Pompidou Centre for Modern Art, the Hotel Georgette provides an insight into all the latest artistic trends. From Pop Art to the New Realists, including Land Art and Street Art, everything is about collections and original ideas.",
+    image: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/03/8a/80/32/hotel-georgette.jpg?w=1600&h=-1&s=1",
+    address: "36 rue du Grenier Saint Lazare, 75003 Paris France"
+},
+
+{
+    id: 4, 
+    name: "Hotel des Arts - Montmartre",
+    description: "Looking for a place to stay in Paris? Then look no further than Hotel des Arts - Montmartre, a romantic hotel that brings the best of Paris to your doorstep. Hotel des Arts Paris, Montmartre, offers guests an array of room amenities including a flat screen TV, a desk, and blackout curtains, and getting online is possible, as free wifi is available and Air Conditioning in all the room with personal command. The hotel offers a 24 hour front desk, a concierge, and baggage storage, to make your visit even more pleasant. The property also features breakfast. Guests arriving by vehicle have access to parking. Close to some of Paris' most popular landmarks, such as Champs-Elysees (1.7 mi) and Louvre Museum (1.7 mi), Hotel Des Arts is a great destination for tourists. ",
+    image: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/23/19/ce/hotel-des-arts-montmartre.jpg?w=1400&h=-1&s=1",
+    address: "5 rue Tholoze, 75018 Paris France"
+},
+
+{
+    id: 5, 
+    name: "Hotel Darcet",
+    description: "Clean, modern, convenient, efficient and charming (air conditioning, parquet floor in all rooms, free internet connection throughout the hotel WIFI, double glazed windows with shutters etc ...)We are located in one of the most attractive and picturesque area of the capital, near the Place de Clichy in the Batignolles district at the foot of Montmartre.Darcet hotel is within walking distance of the Champs Elysees and the Louvre.We are close to the Saint Lazare train station and close enough to transit the Gare du Nord, de L'est, Montparnasse, Austerlitz, Bercy etc ...).",
+    image: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0b/41/2a/aa/street--v11463375.jpg?w=1600&h=-1&s=1",
+    address: "4 rue Darcet 17 Arr., 75017 Paris France"
+},
+
+
+]
+
+export const info = [
+    {
+        number: 1,
+        place: "Le Bistrot Flaubert",
+        restaurant: "https://cdn.vox-cdn.com/thumbor/lbCdF5NE4azU_TJ0ajy-grPThSg=/0x0:800x450/1020x765/filters:focal(374x156:502x284):format(webp):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/69955233/flaubert.0.png",
+    },
+
+    {
+        number: 2,
+        place: "Comice",
+        restaurant: "https://cdn.vox-cdn.com/thumbor/iuLkT7zs0S5IBmZH2eiOdjqT2Dc=/0x0:1080x747/1020x765/filters:focal(454x288:626x460):format(webp):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/62565863/35145200_1016301748535631_6415537283058892800_o.0.0.jpg",
+    },
+
+    {
+        number: 3,
+        place: "Restaurant Arnaud Nicolas",
+        restaurant: "https://cdn.vox-cdn.com/thumbor/UzOvwk3inKQ8Okw8u0AH0vEDdSM=/0x0:1080x1080/1020x765/filters:focal(502x400:674x572):format(webp):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/66331085/Arnaud_FB.0.jpg",
+    },
+
+    {
+        number: 4,
+        place: "Chez L'Ami Jean",
+        restaurant: "https://cdn.vox-cdn.com/thumbor/WN99UusmSYLwVhCNi94NgiwRC90=/0x0:1920x1275/1020x765/filters:focal(936x491:1242x797):format(webp):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/66331086/lami_jean.0.jpg",
+    },
+
+    {
+        number: 5,
+        place: "Restaurant David Toutain",
+        restaurant: "https://cdn.vox-cdn.com/thumbor/cQaj0c25hfOeQtqCtuwIhrn38hA=/0x0:4367x2747/1020x765/filters:focal(1835x1025:2533x1723):format(webp):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/62565855/Restaurant-David-Toutain-official.0.0.0.jpg",
+    }
+]
